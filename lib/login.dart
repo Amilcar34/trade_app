7a0497d9702a1a8050d7106ec290c7128274d0a8
Amilcar34 <alberino-l@hotmail.com>
2018-07-02 10:42:56 -0300

    logn sin imagen de fondo

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validate/validate.dart';
 
 class Login extends StatefulWidget {
   Login({Key key}) : super(key: key);
@@ -10,70 +11,62 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final white = new TextStyle(color: Colors.white);
-    return new Scaffold(
-      body: new Container(
-        decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: new AssetImage("assets/login.jpeg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: new Form(
-            key: _formKey,
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new Text('Trade',
-                  style: new TextStyle(fontSize:62.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: "Merriweather"
-                  ),
-                ),
-                new TextFormField(
-                  style: white,
-                  decoration: InputDecoration(
-                    labelText: 'Usuario', labelStyle: white),
-                  validator: (v){
-                    if(v.isEmpty) return 'Ingrese su usuario';
-                    },
-
-                ),
-                new TextFormField(
-                  style: white,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Contraseña', labelStyle: white,
-                  ),
-                  validator: (v){
-                    if(v.isEmpty) return 'Ingrese su contraseña';
-                  },
-                ),
-                new Container(
-                  width: 200.0,
-                  child: new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    child: RaisedButton(
-                      color: Colors.white,
-                      onPressed: (){
-                        if(_formKey.currentState.validate())
-                          Scaffold.of(context).showSnackBar(SnackBar(content: new Text('Conectando')));
-                      },
-                      child: Text('INGRESAR', style: TextStyle(color: Colors.teal),),
-                    ),
-                  ),
-                  margin: new EdgeInsets.only(
-                      top: 20.0
-                  ),
-                )
-
-              ],
+    final white = new TextStyle(color: Colors.black);
+    return new Form(
+      key: _formKey,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Text('Trade',
+            style: new TextStyle(fontSize:60.0,
+                color: Colors.teal,
+                fontWeight: FontWeight.w600,
+                fontFamily: "Merriweather"
             ),
-        ),
+          ),
+          new TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            style: white,
+            decoration: InputDecoration(
+              labelText: 'Usuario', labelStyle: white),
+            validator: (v) {
+              if (v.isEmpty) return 'Ingrese su usuario';
+            }
+          ),
+          new TextFormField(
+            style: white,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Contraseña', labelStyle: white,
+            ),
+            validator: (v){
+              if(v.isEmpty) return 'Ingrese su contraseña';
+            },
+          ),
+          new Container(
+            width: 200.0,
+            child: new Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: RaisedButton(
+                color: Colors.white,
+                onPressed: (){
+                  if(_formKey.currentState.validate())
+                    Scaffold.of(context).showSnackBar(SnackBar(content: new Text('Conectando')));
+                },
+                child: Text('INGRESAR', style: TextStyle(color: Colors.teal),),
+              ),
+            ),
+            margin: new EdgeInsets.only(
+                top: 20.0
+            ),
+          )
+
+        ],
       ),
-    );
+  );
+
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Generated App',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
-        canvasColor: const Color(0xFFfafafa),
-      ),
-      home: new Login(),
+    Scaffold scaffold = new Scaffold(
+      body: Login(),
     );
+    MaterialApp materialApp = MaterialApp(
+      home: scaffold,
+    );
+    return materialApp;
   }
 }
 
